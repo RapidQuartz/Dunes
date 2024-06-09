d668aa463bc5540f4424651c6245240a9b3d64f8
RapidQuartz <167242038+RapidQuartz@users.noreply.github.com>
2024-06-09 17:25:17 +0000

    Pruning and suchlike

--- 42002/printf/.raw/sewingroom/knidl.sh
+++ 42002/printf/.raw/sewingroom/knidl.sh
@@ -3,28 +3,14 @@
 ###
 ##
 #
-var=1
+var=-1
 num=0
 ite=0
 neg=-1
 global_power="on"
 global_latch="off"
 #
-u_init()
-u_exit()
-#
-d_user()
-d_menu()
-d_event()
-d_twist()
-#
-m_intro()
-m_user()
-m_event()
-m_twist()
-m_outro()
-#
-e_rr()
+usr="default"
 #
 ##
 ###
@@ -32,63 +18,94 @@ e_rr()
 #
 
 #
-u_init()
+u_init ()
 {
+	if [ $global_latch = "on" ] ; then
+		cd ~/42/!Dunes || mkdir ~/42/!Dunes || u_err
+	fi
+	m_intro
+}
+u_exit ()
+{
+    echo "the end" ; exit
+}
+#
+d_user ()
+{
+    if ! [ $usr = "default" ] ; then echo "greetings $usr" && m_user ; fi
+	if [ $usr = "default" ] ; then echo "enter your name" && read -r usr ; d_user ; fi
+}
 
-}
-u_exit()
+d_menu ()
 {
-    exit
+    echo "your choice indicates you wish to know more"
+	path=(pwd)
+	echo "$path"
+	u_exit
+}
+
+d_event ()
+{
+    u_exit
+}
+
+d_twist ()
+{
+    u_exit
 }
 #
-d_user()
+
+m_intro ()
 {
-    
+	echo "[intro menu]"
+	echo "[1 - user]" ; 	echo "[2 - event]" ; 	echo "[3 - twist]"
+	read -r var
+	echo "$var"
+	if [ $var = "1" ] ; then
+		echo "1/1" ; d_user
+    elif [ $var = "2" ] ; then
+		echo "1/2" ; d_event
+    elif [ $var = "3" ] ; then
+		echo "1/3" ; d_twist
+	elif [ $var = "" ] || [ $var = -1 ] ; then
+		echo "1/4" ; d_menu
+	else echo "1/5" ; e_rr	
+	fi
 }
-d_menu()
+
+m_user ()
 {
-    
+	echo "feature creep. Sorry."
 }
-d_event()
-{
+
+# m_event ()
+# {
     
-}
-d_twist()
-{
+# }
+
+# m_twist ()
+# {
     
-}
+# }
+
+# m_outro ()
+# {
+    
+# }
 #
+
+e_rr ()
 {
-    
+	echo "seems something has gone wrong here."
+	if [ $global_power = "off" ] || [ $global_latch = "off" ] ; then
+		echo "let's see how wrong"
+	elif [ $global_power = "off" ] || [ $global_latch = "on" ] ; then 
+		echo "oh… oh dear…"
+	fi
+	echo "nothing for it. Exiting."
+	u_exit
 }
-m_intro()
-{
-    
-}
-m_user()
-{
-    
-}
-m_event()
-{
-    
-}
-m_twist()
-{
-    
-}
-m_outro()
-{
-    
-}
-#
-{
-    
-}
-e_rr()
-{
-    
-}
+
 #
 #
 ##
---
+++ 42002/printf/.raw/sewingroom/seed.sh
@@ -0,0 +1,117 @@
+# #to create folders needed to split printf
+# # //[new structure]
+# # ////////	TEMP:
+# # ////main()	//for testing and calling the function pre-release	
+# # ////////	ROOT:
+# # ////ft_printf()		//the star of the show
+# # ////ft_ptfcha()		//prints individual characters
+# # ////ft_ptfstr()		//prints strings of characters
+# # ////ft_ptfhex_put()		//practical printer of hex
+# # ////////	UTIL:
+# # ////ft_ptfvex()		//checks for flags and selects next ft
+# # ////ft_uitoa()		//converts unsigned int to a string
+# # ////ft_ptfnumlen()		//length-measure for int printing
+# # ////ft_ptfhex_len()		//length-measure of hex number
+# # ////////	SRC:
+# # ////ft_ptfint()		//motherfunction for int printing
+# # ////ft_ptfuin()		//motherfunction for uint printing
+# # ////ft_ptfptr()		//motherfunction for pointer printing
+# # ////ft_ptfhex()		//motherfunction for hex printing
+# # ////////	BONUS:
+# # ////()		//
+# # ////()		//
+# ft_printf
+# ft_ptfcha
+# ft_ptfstr
+# ft_ptfhex_put
+# ft_ptfvex
+# ft_uitoa
+# ft_ptfnumlen
+# ft_ptfhex_len
+# ft_ptfint
+# ft_ptfuin
+# ft_ptfptr
+# ft_ptfhex
+# # ////()		//
+# /$sub_dir
+# ft_printf.c
+# Makefile
+# # ////()		//
+# /$sub_dir/LIB
+# ft_printf.h
+# libft.h
+# Makefile
+# #misc lib functions
+# # ////()		//
+# /$sub_dir/SRC
+# ft_ptfint.c
+# ft_ptfuin.c
+# ft_ptfptr.c
+# ft_ptfhex.c
+# ft_ptfcha.c
+# ft_ptfstr.c
+# # ////()		//
+# /$sub_dir/UTL
+# ft_uitoa.c
+# ft_ptfvex.c
+# ft_ptfhex_put.c
+# ft_ptfnumlen.c
+# ft_ptfhex_len.c
+# # ////()		//
+# # ////	NOTES: [this setup is symmetrical, and mirrors the intended 4 x 4 array]
+
+d_user ()
+{
+    
+    echo "the following files are part of the creation" ; echo "" ; echo "" 
+    if [] ; then d_event
+    else    u_exit
+    fi
+}
+
+u_init ()
+{
+    echo $(pwd)
+    echo $(ls)
+    m_intro
+    u_exit
+}
+
+u_exit ()
+{
+    exit
+}
+
+m_intro ()
+{
+    echo "      based on the previous information"
+    echo "would you like to go ahead with basic touching?"
+    echo " "
+    echo "                  y / n"
+    read -r vm_intro
+    if ! [ $vm_intro = "y"] || ! [ $vm_intro = "n"]
+    u_exit
+    elif [ $vm_intro = "y"] ; then vm_intro="yes"
+        echo "very well, you have selected $vm_intro"
+        d_user
+    elif [ $vm_intro = "n"] ; then vm_intro="no"
+        echo "very well, you have selected $vm_intro"
+        u_exit
+    fi
+}
+
+u_init
+
+# # ////()		//
+# echo ""
+# /$sub_dir
+# ft_printf.c
+# Makefile
+# # ////()		//
+# /$sub_dir/LIB ft_printf.h libft.h Makefile
+# #misc lib functions
+# # ////()		//
+# /$sub_dir/SRC ft_ptfint.c ft_ptfuin.c ft_ptfptr.c ft_ptfhex.c ft_ptfcha.c ft_ptfstr.c
+# # ////()		//
+# /$sub_dir/UTL ft_uitoa.c ft_ptfvex.c ft_ptfhex_put.c ft_ptfnumlen.c ft_ptfhex_len.c
+# # ////()		//
