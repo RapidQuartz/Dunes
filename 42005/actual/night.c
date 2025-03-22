3b8c189b91dda58f885b7069f35cfc3b79a4e066
RapidQuartz <qycksnd@protonmail.com>
2025-03-22 19:12:58 +0100

    ok

---
+++ 42005/actual/night.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   night.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akjoerse <akjoerse@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/22 18:55:45 by akjoerse          #+#    #+#             */
+/*   Updated: 2025/03/22 19:12:24 by akjoerse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_night(int i, char **c)
+{
+	if (i < 3)
+		return ;
+	// index
+	// display
+	// kpi
+}
+
+void	ft_gibberish(int **i, char **c)
+{
+	int	s;//sentence
+	int	o;//objective
+	int	l;//legion
+	int	a;//alphanum
+	
+	if (i != NULL)
+	{
+		ft_gib_solver(i[l], NULL);
+		s = 0;
+		o = 0;
+		l = 0;
+		a = 0;
+		if (c != NULL)
+			ft_gib_solver(NULL, c[s]);
+		ft_assign_performance(i, c);
+		ft_put_touch(i, c);
+	}
+	return ;
+}
