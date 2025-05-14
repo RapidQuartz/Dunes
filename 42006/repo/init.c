5ab1dc4bb38006c86bd6f61509ae1e6a403cce93
RapidQuartz <qycksnd@protonmail.com>
2025-05-14 14:40:14 +0200

    ok keep it steady, you were working on set map point and you were close. maybe integrate some stuff. its gonna be fine. you got this king.

--- 42006/repo/init.c
+++ 42006/repo/init.c
@@ -6,7 +6,7 @@
 /*   By: akjoerse <akjoerse@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 21:52:37 by akjoerse          #+#    #+#             */
-/*   Updated: 2025/05/14 13:08:24 by akjoerse         ###   ########.fr       */
+/*   Updated: 2025/05/14 14:35:54 by akjoerse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,24 +67,75 @@ void	read_map_file(t_fdf *fdf)
 void	split_map_str(t_fdf *fdf)
 {
 	char	**split_map;
-	int	i;
+	int	l;
 	int	j;
 
-	i = 0;
+	l = 0;
 	j = 0;
 	split_map = malloc(sizeof(char *) * fdf->dim->m_y);
 	if (!split_map || split_map == NULL)
 		error ();
 	split_map = ft_split(fdf->map->raw_str, '\n');
-	while (split_map[i])
+	while (split_map[l])
 	{
 		j = 0;
-		fdf->map->elements[i] = ft_split(split_map[i], ' ');
-		while (i == 0 && fdf->map->elements[i][fdf->dim->m_x])
+		fdf->map->elements[l] = ft_split(split_map[l], ' ');
+		while (l == 0 && fdf->map->elements[l][fdf->dim->m_x])
 			fdf->dim->m_x++;
-		while (fdf->map->elements[i][j])
-			printf("[%s]", fdf->map->elements[i][j++]);
+		while (fdf->map->elements[l][j])
+			printf("[%s]", fdf->map->elements[l][j++]);
+		split_map_color(fdf->map->elements[l], l, fdf);
 		printf("\n");
-		i++;
+		l++;
 	}
+}
+
+void	split_map_color(char **map, int l, t_fdf *fdf)
+{
+	int	i;
+	int	j;
+	int	k;
+	
+	i = 0;
+	j = 0;
+	while (map[j][i]);
+	{
+		if (ft_isdigit(map[j][i]) || map[j][i] == '-')
+		{
+			k = 0;
+			while (ft_isdigit(map[j][i]) || map[j][i] == '-')
+				i++;
+			set_map_point(fdf, i, j, l);
+		}
+		if (map[j][i] == ',')
+			set_map_color(map, j, i + 3, fdf);
+		else
+			set_map_color(NULL, j, 0, fdf);
+		j++;
+		i = 0;
+	}
+}
+void	set_map_point(t_fdf *fdf, int k, int r, int l)
+{
+	char	*lmnt;
+	int	numb;
+	int	i;
+	
+	i = -1;
+	lmnt = malloc(sizeof(char) * k);
+	if (!lmnt || lmnt == NULL)
+		error();
+	while (++i < k)
+		lmnt[i] = fdf->map->elements[l][r][i];
+	numb = ft_atoi(lmnt);
+	fdf->map->points[l][r] = numb;
+}
+void	set_map_color(char **map, int j, int i, t_fdf *fdf)
+{
+	
+	char	*color;
+	if (i == 0 || map == NULL)
+		color = DEFCOL;
+	else
+		color = ft_strdup
 }
